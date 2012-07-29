a86bcf6fed560c8d30d05471790dd46963b4a04b
John Murphy <smurfier20@gmail.com>
2012-07-29 15:59:10 -0500

    Method change. Added User Input Lists.

--- RainLaunch/@Resources/Run.lua
+++ RainLaunch/@Resources/Run.lua
@@ -1,18 +1,19 @@
 function Initialize()
+	Timer = -1
 	local file = io.input(SKIN:GetVariable('@') .. 'Run.cfg')
 	Execute = {}
 	Search = {}
 	local section
 	if io.type(file) == 'file' then
 		for line in io.lines() do
-			if not string.match(line,'^;')then
+			if not string.match(line, '^;')then
 				local key,command = string.match(line, '^([^=]+)=(.+)')
 				if string.match(line, '^%s-%[.+') then
 					section = string.lower(string.match(line, '^%s-%[([^%]]+)'))
 				elseif key and command and section then
 					local nkey = string.lower(key)
 					if section == 'search' then
-						Search[nkey]=command
+						Search[nkey] = command
 					elseif section == 'macros' then
 						Execute[nkey] = command
 					end
@@ -23,44 +24,78 @@ function Initialize()
 	end
 end
 
+function Update()
+	if Timer > 0 then
+		Timer = Timer-1
+	elseif Timer == 0 then
+		Timer = -1
+		SKIN:Bang('!SetVariable', 'Output', 'Run...')
+		SKIN:Bang('!DisableMeasure', 'Lua')
+	end
+end
+
 function Run()
-	SKIN:Bang('!SetVariable','Output','Run...')
+	SKIN:Bang('!SetVariable', 'Output', 'Run...')
+	Timer = -1
 	local command = SKIN:GetVariable('Run')
-	local func,term = string.match(command, '^([^%s]+) (.+)')
-	func = string.lower(func or '')
+	local args = {}
+	for word in string.gmatch(command, '[^%s]+') do table.insert(args, word) end
 	
-	if Search[func] and term then
-		local text = string.gsub(term, '%s', '%%%%20')
-		local line = string.gsub(Search[func], '%$UserInput%$', term)
-		SKIN:Bang('"'..line..'"')
-	elseif func == 'web' and term then
-		local tbl = {}
-		for word in string.gmatch(term, '[^%.]+') do table.insert(tbl, word) end
-		SKIN:Bang('"http://'..(#tbl>=3 and '' or 'www.')..table.concat(tbl,'.')..(#tbl>=2 and '"' or '.com"'))
-	elseif func == 'calc' and term then
-		local value = SKIN:ParseFormula('('..term..')')
-		SKIN:Bang('!SetVariable', 'Output', value)
-	elseif Execute[func] and term then
-		if string.match(Execute[func],'\\%d') then
-			local test = 0
-			for num in string.gmatch(Execute[func], '\\(%d)') do if tonumber(num)>test then test=tonumber(num) end end
+	if #args>0 then
+		local func = string.lower(table.remove(args, 1))
+		if Search[func] and #args > 0 then
+			local text = table.concat(args, '%%20')
+			local line = string.gsub(Search[func], '\\1', text)
+			SKIN:Bang('"'..line..'"')
+		elseif func == 'web' and #args > 0 then
 			local tbl = {}
-			for word in string.gmatch(term, '[^%s]+') do table.insert(tbl,word) end
-			if #tbl < test then
-				SKIN:Bang('!SetVariable','OutPut',func..': Not Enough Parameters')
+			for word in string.gmatch(table.concat(args,'%%20'), '[^%.]+') do table.insert(tbl, word) end
+			SKIN:Bang('"http://'..(#tbl >= 3 and '' or 'www.')..table.concat(tbl,'.')..(#tbl >= 2 and '"' or '.com"'))
+		elseif func == 'calc' and #args > 0 then
+			local value = SKIN:ParseFormula('('..table.concat(args)..')')
+			Output(value)
+		elseif Execute[func] and #args > 0 then
+			if string.match(Execute[func],'\\%d') then
+				local test = 0
+				for num in string.gmatch(Execute[func], '\\(%d)') do if tonumber(num) > test then test=tonumber(num) end end
+				if #args < test then
+					Output(func..': Not Enough Parameters')
+				else
+					local err = false
+					local text = string.gsub(Execute[func], '\\(%d)(%b{})', function(num, list)
+						local par = tonumber(num) == test and table.concat(args, ' ', test) or (args[tonumber(num)] or '\\'..num)
+						for word in string.gmatch(list, '[^%|{}]+') do
+							local w,p = string.lower(word),string.lower(par)
+							if w == p or string.match(p, w) then
+								return par
+							else
+								err = true
+							end
+						end
+					end)
+					text = string.gsub(text, '\\(%d)', function(num, list)
+						return tonumber(num) == test and table.concat(args, ' ', test) or (args[tonumber(num)] or '\\'..num)
+					end)
+					if not err then
+						SKIN:Bang(text)
+					else
+						Output(func..': Invalid Parameter')
+					end
+				end
 			else
-				local text = string.gsub(Execute[func], '\\(%d)', function(a)
-					return tonumber(a)==test and table.concat(tbl,' ',test) or (tbl[tonumber(a)] or '')
-				end)
-				SKIN:Bang(text)
+				SKIN:Bang(Execute[string.lower(command)] or command)
 			end
+		elseif string.match(string.lower(command), '^http://') then
+			SKIN:Bang('"'..command..'"')
 		else
 			SKIN:Bang(Execute[string.lower(command)] or command)
 		end
-	elseif string.match(string.lower(command), '^http://') then
-		SKIN:Bang('"'..command..'"')
-	else
-		SKIN:Bang(Execute[string.lower(command)] or command)
 	end
 	SKIN:Bang('!Update')
+end
+
+function Output(outtext)
+	Timer = 300
+	SKIN:Bang('!EnableMeasure', 'Lua')
+	SKIN:Bang('!SetVariable', 'Output', outtext)
 end
